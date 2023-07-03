e6cff2b598ad5edc33d46e8378d00b77a4ee3d30
Dico Diaz <dicodiaz@gmail.com>
2023-07-03 12:56:44 -0500

    Generate Category scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect('/categories', status: 302)
+  resources :categories
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
