37535aa2e9735d18ca2d69e229ae0b6bcfda9146
Dico Diaz <dicodiaz@gmail.com>
2023-07-06 11:47:31 -0500

    Add models specs

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'user name', email: 'user_email@mail.com', password: 'user_password')
+    @purchase = Purchase.create(name: 'purchase name', amount: 1.0, author: @user)
+    @category = Category.create(name: 'category name', icon: 'category icon', user: @user)
+    @purchase.categories << @category
+  end
+
+  context 'validations' do
+    it('should be valid') do
+      expect(@purchase).to be_valid
+    end
+
+    it 'should be invalid when name is not present' do
+      @purchase.name = nil
+      expect(@purchase).not_to be_valid
+    end
+
+    it 'should be invalid when amount is not present' do
+      @purchase.amount = nil
+      expect(@purchase).not_to be_valid
+    end
+
+    it 'should be invalid when no category is present' do
+      @purchase.categories.clear
+      expect(@purchase).not_to be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'user name', email: 'user_email@mail.com', password: 'user_password') }
+
+  context '#select_categories' do
+    before(:each) do
+      @category = Category.create(name: 'category name', icon: 'category icon', user:)
+      @category2 = Category.create(name: 'category2 name', icon: 'category2 icon', user:)
+    end
+
+    it 'should return an array with a title as first element and an information array as second element' do
+      categories = user.select_categories
+      expect(categories).to eq([['Categories', [[@category.name, @category.id], [@category2.name, @category2.id]]]])
+    end
+  end
 end
